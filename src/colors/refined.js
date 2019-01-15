ef2800b20cd0f3cacc468eb3aef871c4f78047f3
Jon Q <hello@jonquach.com>
2019-01-15 18:41:32 -0500

    Revert "Toned down yellow to match closer to Dark+"
    This reverts commit b0698cb054b1c5413fb39cb95a1c621f5fc972ab.

--- src/colors/refined.js
+++ src/colors/refined.js
@@ -4,10 +4,7 @@ const colors = {
   ...baseColors,
   green: "#addb7b",
   cyan: "#78ccf0",
-  brightCyan: "#78ccf0",
-  yellow: "#e6d2aa",
-  gold: "#e6d2aa",
-  brightYellow: "#e6d2aa"
+  brightCyan: "#78ccf0"
 };
 
 module.exports = colors;
