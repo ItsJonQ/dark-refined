b0698cb054b1c5413fb39cb95a1c621f5fc972ab
Jon Q <hello@jonquach.com>
2019-01-15 18:29:53 -0500

    Toned down yellow to match closer to Dark+

--- src/colors/refined.js
+++ src/colors/refined.js
@@ -4,7 +4,10 @@ const colors = {
   ...baseColors,
   green: "#addb7b",
   cyan: "#78ccf0",
-  brightCyan: "#78ccf0"
+  brightCyan: "#78ccf0",
+  yellow: "#e6d2aa",
+  gold: "#e6d2aa",
+  brightYellow: "#e6d2aa"
 };
 
 module.exports = colors;
